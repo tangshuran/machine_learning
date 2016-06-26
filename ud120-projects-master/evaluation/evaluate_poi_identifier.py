364ca3d658bac0faea76028a8e59e5f7882f21f9
tang_777 <tangshuran2@gmail.com>
2016-06-26 16:42:18 +0200

    ok

--- ud120-projects-master/evaluation/evaluate_poi_identifier.py
+++ ud120-projects-master/evaluation/evaluate_poi_identifier.py
@@ -23,9 +23,32 @@ features_list = ["poi", "salary"]
 
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
+from sklearn.cross_validation import train_test_split
+feature_train, feature_test, label_train, label_test = train_test_split(features, labels, test_size=0.3, random_state=42)
 
 
-
+### it's all yours from here forward!  
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(feature_train, label_train)
+pred=clf.predict(feature_test)
 ### your code goes here 
+from sklearn.metrics import precision_score,recall_score
+precision=precision_score(pred,label_test)
 
+o=0
+predictions = [0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1]
+true_labels = [0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0]
 
+for i,a in enumerate(predictions):
+    if a ==1.0:
+        if true_labels[i]==1.0:
+            o=o+1
+q=0
+predictions = [0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1] 
+true_labels = [0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0]
+for i,a in enumerate(predictions):
+    if a ==1.0:
+        if true_labels[i]==0:
+            q=q+1
