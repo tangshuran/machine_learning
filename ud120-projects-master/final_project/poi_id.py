c3fd5403212d9a0f8cda588d7973904c533b5cf8
Tang777 <tangshuran@users.noreply.github.com>
2016-06-27 16:39:45 +0200

    find the outlier

--- ud120-projects-master/final_project/poi_id.py
+++ ud120-projects-master/final_project/poi_id.py
@@ -2,7 +2,13 @@
 
 import sys
 import pickle
-sys.path.append("../tools/")
+import os
+import numpy as np
+from scipy.stats import describe
+import matplotlib.pyplot as plt
+
+os.chdir(r"E:\machine_learning\ud120-projects-master\final_project")
+sys.path.append(r"E:\machine_learning\ud120-projects-master/tools/")
 
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
@@ -10,13 +16,42 @@ from tester import dump_classifier_and_data
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary'] # You will need to use more features
+features_list = ['poi','salary',  'from_poi_to_this_person', 'from_messages', 'from_this_person_to_poi', 'shared_receipt_with_poi',
+'total_payments', 'loan_advances', 'bonus', 'restricted_stock_deferred', 'deferred_income', 'total_stock_value', 'expenses'] # You will need to use more features
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
-
+def summarise_data(feature,data=data_dict):
+    num_of_nans=0
+    temp=[]
+    for people,info in data.iteritems():
+        if info[feature]=="NaN":
+            num_of_nans +=1
+        else:
+            temp.append(info[feature])
+    result=describe(temp)
+    print "num_of_nans:",num_of_nans
+    print ""
+    print "describe the feature:",result
+        
+    
+    
 ### Task 2: Remove outliers
+#find the existance of the outlier through visualization
+def pre_visualization(column1,column2,data=data_dict):
+    x=[]
+    y=[]
+    for people,info in data.iteritems():
+        if info[column1]!="NaN" and info[column2]!="NaN":
+            x.append(info[column1])
+            y.append(info[column2])
+    return x,y
+x,y=pre_visualization("salary",'bonus')
+plt.plot(x,y,".")
+plt.show() #we can see that there is a point at the top right part of the plot, that is a outlier
+#next thing to do is to find the outlier, and delete it
+
 ### Task 3: Create new feature(s)
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
@@ -44,8 +79,7 @@ clf = GaussianNB()
 
 # Example starting point. Try investigating other evaluation techniques!
 from sklearn.cross_validation import train_test_split
-features_train, features_test, labels_train, labels_test = \
-    train_test_split(features, labels, test_size=0.3, random_state=42)
+features_train, features_test, labels_train, labels_test =train_test_split(features, labels, test_size=0.3, random_state=42)
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
